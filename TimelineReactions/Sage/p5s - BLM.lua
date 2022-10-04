626784969017ea13af8a56e7984c1b9c6641217f
SageRhaposdy <51897297+SageRhaposdy@users.noreply.github.com>
2022-10-04 13:57:37 -0400

    Add files via upload

---
+++ TimelineReactions/Sage/p5s - BLM.lua
@@ -0,0 +1,742 @@
+local tbl = 
+{
+	[8] = 
+	{
+		
+		{
+			data = 
+			{
+				actions = 
+				{
+					
+					{
+						data = 
+						{
+							aType = "Lua",
+							actionLua = "SallyBLM.HotBarConfig.Sharpcast.enabled = false\nself.used = true",
+							uuid = "e587d8c1-a969-278f-9861-ddde9f897709",
+							version = 2,
+						},
+					},
+				},
+				conditions = 
+				{
+				},
+				mechanicTime = 56.3,
+				name = "sharp",
+				timelineIndex = 8,
+				timerOffset = -8,
+				uuid = "77a8c888-d16f-af77-8e23-c1fdd48f26c7",
+				version = 2,
+			},
+		},
+	},
+	[10] = 
+	{
+		
+		{
+			data = 
+			{
+				actions = 
+				{
+					
+					{
+						data = 
+						{
+							aType = "Lua",
+							actionLua = "SallyBLM.HotBarConfig.Sharpcast.enabled = false\nself.used = true",
+							uuid = "e587d8c1-a969-278f-9861-ddde9f897709",
+							version = 2,
+						},
+					},
+				},
+				conditions = 
+				{
+				},
+				mechanicTime = 72.1,
+				name = "sharp",
+				timelineIndex = 10,
+				timerOffset = 5,
+				uuid = "c3c6023e-9c77-41ba-a39c-740faa3b731a",
+				version = 2,
+			},
+		},
+	},
+	[12] = 
+	{
+		
+		{
+			data = 
+			{
+				actions = 
+				{
+					
+					{
+						data = 
+						{
+							aType = "Lua",
+							actionLua = "SallyBLM.HotBarConfig.Sharpcast.enabled = false\nself.used = true",
+							uuid = "e587d8c1-a969-278f-9861-ddde9f897709",
+							version = 2,
+						},
+					},
+				},
+				conditions = 
+				{
+				},
+				mechanicTime = 101.3,
+				name = "sharp",
+				timelineIndex = 12,
+				timerOffset = -8,
+				uuid = "55f0cc50-a641-a256-a647-170cb586753f",
+				version = 2,
+			},
+		},
+	},
+	[14] = 
+	{
+		
+		{
+			data = 
+			{
+				actions = 
+				{
+					
+					{
+						data = 
+						{
+							aType = "Lua",
+							actionLua = "SallyBLM.HotBarConfig.Sharpcast.enabled = false\nself.used = true",
+							uuid = "e587d8c1-a969-278f-9861-ddde9f897709",
+							version = 2,
+						},
+					},
+				},
+				conditions = 
+				{
+				},
+				mechanicTime = 122.7,
+				name = "sharp",
+				timelineIndex = 14,
+				uuid = "7bf452d8-ad4c-cd52-b4ac-b948c5dd1d16",
+				version = 2,
+			},
+		},
+		
+		{
+			data = 
+			{
+				actions = 
+				{
+					
+					{
+						data = 
+						{
+							aType = "Lua",
+							actionLua = "SallyBLM.HotBarConfig.LeyLines.enabled = false\nself.used = true",
+							uuid = "e587d8c1-a969-278f-9861-ddde9f897709",
+							version = 2,
+						},
+					},
+				},
+				conditions = 
+				{
+				},
+				mechanicTime = 122.7,
+				name = "ley",
+				timelineIndex = 14,
+				timerOffset = 3,
+				uuid = "77b66e45-b842-9fb2-90f8-2aff23f94615",
+				version = 2,
+			},
+		},
+	},
+	[18] = 
+	{
+		
+		{
+			data = 
+			{
+				actions = 
+				{
+					
+					{
+						data = 
+						{
+							aType = "Lua",
+							actionLua = "SallyBLM.HotBarConfig.Surecast.enabled = false\nself.used = true",
+							uuid = "d3016a03-a35f-7346-8475-b7bebc935741",
+							version = 2,
+						},
+					},
+				},
+				conditions = 
+				{
+				},
+				mechanicTime = 135.2,
+				name = "sure",
+				timelineIndex = 18,
+				timerOffset = -4,
+				uuid = "27f9a381-0073-5b6b-9788-443b6eb07d92",
+				version = 2,
+			},
+		},
+	},
+	[23] = 
+	{
+		
+		{
+			data = 
+			{
+				actions = 
+				{
+					
+					{
+						data = 
+						{
+							aType = "Lua",
+							actionLua = "SallyBLM.HotBarConfig.Sharpcast.enabled = false\nself.used = true",
+							uuid = "e587d8c1-a969-278f-9861-ddde9f897709",
+							version = 2,
+						},
+					},
+				},
+				conditions = 
+				{
+				},
+				mechanicTime = 159.6,
+				name = "sharp",
+				timelineIndex = 23,
+				timerOffset = -10,
+				uuid = "d9f2f35a-0ea4-197c-9441-f589664f26ea",
+				version = 2,
+			},
+		},
+	},
+	[33] = 
+	{
+		
+		{
+			data = 
+			{
+				actions = 
+				{
+					
+					{
+						data = 
+						{
+							aType = "Lua",
+							actionLua = "SallyBLM.HotBarConfig.Sharpcast.enabled = false\nself.used = true",
+							uuid = "e587d8c1-a969-278f-9861-ddde9f897709",
+							version = 2,
+						},
+					},
+				},
+				conditions = 
+				{
+				},
+				mechanicTime = 193.1,
+				name = "sharp",
+				timelineIndex = 33,
+				timerOffset = -8,
+				uuid = "9d7e9548-4607-79e8-a9a1-ede00fc2bea1",
+				version = 2,
+			},
+		},
+	},
+	[38] = 
+	{
+		
+		{
+			data = 
+			{
+				actions = 
+				{
+					
+					{
+						data = 
+						{
+							aType = "Lua",
+							actionLua = "SallyBLM.HotBarConfig.Sharpcast.enabled = false\nself.used = true",
+							uuid = "e587d8c1-a969-278f-9861-ddde9f897709",
+							version = 2,
+						},
+					},
+				},
+				conditions = 
+				{
+				},
+				mechanicTime = 224.5,
+				name = "sharp",
+				timelineIndex = 38,
+				timerOffset = -8,
+				uuid = "d315ac31-b26f-25c8-84c9-aec7c864c594",
+				version = 2,
+			},
+		},
+	},
+	[42] = 
+	{
+		
+		{
+			data = 
+			{
+				actions = 
+				{
+					
+					{
+						data = 
+						{
+							aType = "Lua",
+							actionLua = "SallyBLM.HotBarConfig.TripleCast.enabled = false\nself.used = true",
+							uuid = "e587d8c1-a969-278f-9861-ddde9f897709",
+							version = 2,
+						},
+					},
+				},
+				conditions = 
+				{
+				},
+				mechanicTime = 256.1,
+				name = "Triple",
+				timelineIndex = 42,
+				timerOffset = -10,
+				uuid = "7b5d2a02-154a-5e87-b13f-e57aeded3d49",
+				version = 2,
+			},
+		},
+		
+		{
+			data = 
+			{
+				actions = 
+				{
+					
+					{
+						data = 
+						{
+							aType = "Lua",
+							actionLua = "SallyBLM.QTBar.BurnBoss.enabled = false\nself.used = true",
+							uuid = "19f4fb4b-e509-6c83-abe0-e8974c94341b",
+							version = 2,
+						},
+					},
+				},
+				conditions = 
+				{
+				},
+				mechanicTime = 256.1,
+				name = "burn",
+				timelineIndex = 42,
+				uuid = "1c77c48c-629e-2b07-8bb4-09abe2debcfc",
+				version = 2,
+			},
+		},
+	},
+	[50] = 
+	{
+		
+		{
+			data = 
+			{
+				actions = 
+				{
+					
+					{
+						data = 
+						{
+							aType = "Lua",
+							actionLua = "SallyBLM.HotBarConfig.Sharpcast.enabled = false\nself.used = true",
+							uuid = "e587d8c1-a969-278f-9861-ddde9f897709",
+							version = 2,
+						},
+					},
+				},
+				conditions = 
+				{
+				},
+				mechanicTime = 266,
+				name = "sharp",
+				timelineIndex = 50,
+				timerOffset = -8,
+				uuid = "f42979ef-e4e8-a569-b1fb-9af2d15e5875",
+				version = 2,
+			},
+		},
+	},
+	[52] = 
+	{
+		
+		{
+			data = 
+			{
+				actions = 
+				{
+					
+					{
+						data = 
+						{
+							aType = "Lua",
+							actionLua = "SallyBLM.HotBarConfig.LeyLines.enabled = false\nself.used = true",
+							uuid = "e587d8c1-a969-278f-9861-ddde9f897709",
+							version = 2,
+						},
+					},
+				},
+				conditions = 
+				{
+				},
+				mechanicTime = 267,
+				name = "ley",
+				timelineIndex = 52,
+				timerOffset = 3,
+				uuid = "8db563e0-2be8-a1cb-8a40-1ca7239e3d18",
+				version = 2,
+			},
+		},
+	},
+	[61] = 
+	{
+		
+		{
+			data = 
+			{
+				actions = 
+				{
+					
+					{
+						data = 
+						{
+							aType = "Lua",
+							actionLua = "SallyBLM.HotBarConfig.Sharpcast.enabled = false\nself.used = true",
+							uuid = "e587d8c1-a969-278f-9861-ddde9f897709",
+							version = 2,
+						},
+					},
+				},
+				conditions = 
+				{
+				},
+				mechanicTime = 305.9,
+				name = "sharp",
+				timelineIndex = 61,
+				timerOffset = -7,
+				uuid = "39a0a080-8d3b-17fc-b426-439b80cb93c8",
+				version = 2,
+			},
+		},
+	},
+	[68] = 
+	{
+		
+		{
+			data = 
+			{
+				actions = 
+				{
+					
+					{
+						data = 
+						{
+							aType = "Lua",
+							actionLua = "SallyBLM.HotBarConfig.Sharpcast.enabled = false\nself.used = true",
+							uuid = "e587d8c1-a969-278f-9861-ddde9f897709",
+							version = 2,
+						},
+					},
+				},
+				conditions = 
+				{
+				},
+				mechanicTime = 339.2,
+				name = "sharp",
+				timelineIndex = 68,
+				timerOffset = -8,
+				uuid = "0f17307a-e8fc-f622-91d1-74d055b373fc",
+				version = 2,
+			},
+		},
+	},
+	[74] = 
+	{
+		
+		{
+			data = 
+			{
+				actions = 
+				{
+					
+					{
+						data = 
+						{
+							aType = "Lua",
+							actionLua = "SallyBLM.HotBarConfig.TripleCast.enabled = false\nself.used = true",
+							uuid = "e587d8c1-a969-278f-9861-ddde9f897709",
+							version = 2,
+						},
+					},
+				},
+				conditions = 
+				{
+				},
+				mechanicTime = 373.9,
+				name = "Triple",
+				timelineIndex = 74,
+				timerOffset = 1,
+				uuid = "cc994336-2831-72a7-ba67-962d13ce7e47",
+				version = 2,
+			},
+		},
+	},
+	[75] = 
+	{
+		
+		{
+			data = 
+			{
+				actions = 
+				{
+					
+					{
+						data = 
+						{
+							aType = "Lua",
+							actionLua = "SallyBLM.HotBarConfig.Sharpcast.enabled = false\nself.used = true",
+							uuid = "e587d8c1-a969-278f-9861-ddde9f897709",
+							version = 2,
+						},
+					},
+				},
+				conditions = 
+				{
+				},
+				mechanicTime = 377.8,
+				name = "sharp",
+				timelineIndex = 75,
+				timerOffset = -8,
+				uuid = "a761adb1-d2fc-9914-87ff-9a430d7c517d",
+				version = 2,
+			},
+		},
+	},
+	[79] = 
+	{
+		
+		{
+			data = 
+			{
+				actions = 
+				{
+					
+					{
+						data = 
+						{
+							aType = "Lua",
+							actionLua = "SallyBLM.HotBarConfig.Sharpcast.enabled = false\nself.used = true",
+							uuid = "e587d8c1-a969-278f-9861-ddde9f897709",
+							version = 2,
+						},
+					},
+				},
+				conditions = 
+				{
+				},
+				mechanicTime = 404.8,
+				name = "sharp",
+				timelineIndex = 79,
+				timerOffset = -8,
+				uuid = "38664f7e-86b7-e932-8bb1-4cc372c51b42",
+				version = 2,
+			},
+		},
+		
+		{
+			data = 
+			{
+				actions = 
+				{
+					
+					{
+						data = 
+						{
+							aType = "Lua",
+							actionLua = "SallyBLM.HotBarConfig.LeyLines.enabled = false\nself.used = true",
+							uuid = "e587d8c1-a969-278f-9861-ddde9f897709",
+							version = 2,
+						},
+					},
+				},
+				conditions = 
+				{
+				},
+				mechanicTime = 404.8,
+				name = "ley",
+				timelineIndex = 79,
+				timerOffset = 6,
+				uuid = "27f5abd9-9a90-5360-875a-c110ea457c35",
+				version = 2,
+			},
+		},
+	},
+	[82] = 
+	{
+		
+		{
+			data = 
+			{
+				actions = 
+				{
+					
+					{
+						data = 
+						{
+							aType = "Lua",
+							actionLua = "SallyBLM.HotBarConfig.Sharpcast.enabled = false\nself.used = true",
+							uuid = "e587d8c1-a969-278f-9861-ddde9f897709",
+							version = 2,
+						},
+					},
+				},
+				conditions = 
+				{
+				},
+				mechanicTime = 435.4,
+				name = "sharp",
+				timelineIndex = 82,
+				timerOffset = -1,
+				uuid = "1a200c8b-9bd0-0014-ae2d-f7454772e864",
+				version = 2,
+			},
+		},
+	},
+	[88] = 
+	{
+		
+		{
+			data = 
+			{
+				actions = 
+				{
+					
+					{
+						data = 
+						{
+							aType = "Lua",
+							actionLua = "SallyBLM.HotBarConfig.Sharpcast.enabled = false\nself.used = true",
+							uuid = "e587d8c1-a969-278f-9861-ddde9f897709",
+							version = 2,
+						},
+					},
+				},
+				conditions = 
+				{
+				},
+				mechanicTime = 462.8,
+				name = "sharp",
+				timelineIndex = 88,
+				timerOffset = -5,
+				uuid = "fbab141f-cf31-b843-8284-b987f219ab03",
+				version = 2,
+			},
+		},
+		
+		{
+			data = 
+			{
+				actions = 
+				{
+					
+					{
+						data = 
+						{
+							aType = "Lua",
+							actionLua = "SallyBLM.HotBarConfig.TripleCast.enabled = false\nself.used = true",
+							uuid = "e587d8c1-a969-278f-9861-ddde9f897709",
+							version = 2,
+						},
+					},
+				},
+				conditions = 
+				{
+				},
+				mechanicTime = 462.8,
+				name = "Triple",
+				timelineIndex = 88,
+				timerOffset = -1,
+				uuid = "338ce79e-bc48-a30c-84ab-1cce17bcf50d",
+				version = 2,
+			},
+		},
+	},
+	[91] = 
+	{
+		
+		{
+			data = 
+			{
+				actions = 
+				{
+					
+					{
+						data = 
+						{
+							aType = "Lua",
+							actionLua = "SallyBLM.HotBarConfig.Surecast.enabled = false\nself.used = true",
+							uuid = "e587d8c1-a969-278f-9861-ddde9f897709",
+							version = 2,
+						},
+					},
+				},
+				conditions = 
+				{
+				},
+				mechanicTime = 474.7,
+				name = "Sure",
+				timelineIndex = 91,
+				timerOffset = -4,
+				uuid = "cef0b8ca-4a67-56f1-9066-5728ca72664d",
+				version = 2,
+			},
+		},
+	},
+	[94] = 
+	{
+		
+		{
+			data = 
+			{
+				actions = 
+				{
+					
+					{
+						data = 
+						{
+							aType = "Lua",
+							actionLua = "SallyBLM.HotBarConfig.TripleCast.enabled = false\nself.used = true",
+							uuid = "e587d8c1-a969-278f-9861-ddde9f897709",
+							version = 2,
+						},
+					},
+				},
+				conditions = 
+				{
+				},
+				mechanicTime = 489.8,
+				name = "Triple",
+				timelineIndex = 94,
+				timerOffset = 6,
+				uuid = "38563a66-a7a2-e09e-908b-56a007168083",
+				version = 2,
+			},
+		},
+	},
+	inheritedProfiles = 
+	{
+	},
+	mapID = 1082,
+	version = 4,
+}
+
+
+
+return tbl
